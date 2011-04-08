9d4b2d05287387c9ddcc3fe65bcdd6e8c9ee3440
HairyFotr <maticpotocnik@gmail.com>
2011-04-08 19:47:10 +0200

    refactor,camera,proto-skybox

--- src/AngryPigs.scala
+++ src/AngryPigs.scala
@@ -11,7 +11,8 @@ object AngryPigs {
 
     var isRunning = false; // is main loop running
     val (winWidth, winHeight)=(800,600); // window size
-    val rand:Random = new Random;
+    val cam = new Camera;
+    val rand = new Random;
     
     /**
      * Initializes display and enters main loop
@@ -50,8 +51,8 @@ object AngryPigs {
         //Display.create(new PixelFormat(8, 8, 8, 4));
         // No FSAA
         Display.create;
-        Display.setTitle(this.getClass.getName);
-    }     
+        Display.setTitle("Angry Pigs");
+    }
     /**
      * Main loop: renders and processes input events
      */
@@ -60,11 +61,26 @@ object AngryPigs {
         makeModels; // make generative models
         setupView;  // setup camera and lights
 
-        while(isRunning) {            
-            resetView;      // reset view
-            renderFrame;    // let subsystem paint
-            processInput;   // process input events @menda se da sproti gledat
+        // @that is one ugly FPS counter :)
+        def now = System.nanoTime();
+        var secondTimer = now;
+        var frameCounter = 0;
+        val E10 = 10000000000L;
+        while(isRunning) {
+            frameCounter += 1;
+            //val start = now;
+            resetView;      // clear view and reset transformations
+            renderFrame;    // draw stuff
+            // @menda se da sproti/bolš gledat input
+            processInput;   // process input events 
             Display.update; // update window contents and process input messages
+            //val timeSpent = now-start;
+            if(now-secondTimer > E10) {
+                secondTimer = now;
+                // print fps
+                println("FPS: "+frameCounter/10);
+                frameCounter = 0;
+            }
         }
     }
     
@@ -87,20 +103,14 @@ object AngryPigs {
 	 */
     def setupView {
         GL11.glEnable(GL11.GL_DEPTH_TEST); // enable depth buffer (off by default)
-        GL11.glEnable(GL11.GL_CULL_FACE);  // enable culling of back sides of polygons
+        //GL11.glEnable(GL11.GL_CULL_FACE);  // enable culling of back sides of polygons
       
         GL11.glViewport(0,0, winWidth,winHeight); // mapping from normalized to window coordinates
 	   
-        GL11.glMatrixMode(GL11.GL_PROJECTION); // setup projection matrix stack
-        GL11.glLoadIdentity;
-        
-        // orthographic projection (...) 
-        //GL11.glOrtho(-5,5,-5,5,1,30);
-        // perspective projection (FOV, aspect, clipping near, clipping far);
-        GLU.gluPerspective(45, winWidth/winHeight.toFloat, 1.0f, 100.0f);
+        cam.setPerspective(45, 4/3f, 1f, 50f);
+        cam.setPosition(0,0,2f);
     }
   
-
     /**
     * Resets the view of current frame
     */
@@ -109,44 +119,102 @@ object AngryPigs {
         GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
         GL11.glMatrixMode(GL11.GL_PROJECTION);
         GL11.glLoadIdentity;
-        GLU.gluPerspective(45, winWidth/winHeight.toFloat, 1.0f, 100.0f);
-        GL11.glRotatef(10f,rot.x,rot.y,rot.z);
-        GL11.glTranslatef(tran.x,tran.y,tran.z);
         GL11.glMatrixMode(GL11.GL_MODELVIEW);
         GL11.glLoadIdentity;
     }
   
-    // camera rotation and translation
-    // @replace with camera object, ofcourse
-    var (rot,tran)=(new Vec3(17.5f,-10f,-3.3f), new Vec3(-0.9f,-1.5f,-0.5f));//(new Vec3,new Vec3);
- 
     /**
     * Renders current frame
     */
     def renderFrame {
+        cam.render;
         terrain.render;
+        
+        // coordinate system
+        /*GL11.glBegin(GL11.GL_LINES);
+            GL11.glColor3f(1,0,0);
+            GL11.glVertex3f(0,0,0);
+            GL11.glVertex3f(100,0,0);
+            
+            GL11.glColor3f(0,1,0);
+            GL11.glVertex3f(0,0,0);
+            GL11.glVertex3f(0,100,0);
+            
+            GL11.glColor3f(0,0,1);
+            GL11.glVertex3f(0,0,0);
+            GL11.glVertex3f(0,0,100);
+        GL11.glEnd;*/
+        
+        // sky-box
+        
+        GL11.glPushMatrix;
+        GL11.glTranslatef(0,-1,0);
+        GL11.glScalef(5,5,5);
+        GL11.glBegin(GL11.GL_QUADS);
+		    GL11.glColor3f(0.0f,1.0f,0.0f);						// Set The Color To Green
+		    GL11.glVertex3f( 1.0f, 1.0f,-1.0f);					// Top Right Of The Quad (Top)
+		    GL11.glVertex3f(-1.0f, 1.0f,-1.0f);					// Top Left Of The Quad (Top)
+		    GL11.glVertex3f(-1.0f, 1.0f, 1.0f);					// Bottom Left Of The Quad (Top)
+		    GL11.glVertex3f( 1.0f, 1.0f, 1.0f);					// Bottom Right Of The Quad (Top)
+		    
+		    GL11.glColor3f(1.0f,0.5f,0.0f);						// Set The Color To Orange
+		    GL11.glVertex3f( 1.0f,-1.0f, 1.0f);					// Top Right Of The Quad (Bottom)
+		    GL11.glVertex3f(-1.0f,-1.0f, 1.0f);					// Top Left Of The Quad (Bottom)
+		    GL11.glVertex3f(-1.0f,-1.0f,-1.0f);					// Bottom Left Of The Quad (Bottom)
+		    GL11.glVertex3f( 1.0f,-1.0f,-1.0f);					// Bottom Right Of The Quad (Bottom)
+		    
+		    GL11.glColor3f(1.0f,0.0f,0.0f);						// Set The Color To Red
+		    GL11.glVertex3f( 1.0f, 1.0f, 1.0f);					// Top Right Of The Quad (Front)
+		    GL11.glVertex3f(-1.0f, 1.0f, 1.0f);					// Top Left Of The Quad (Front)
+		    GL11.glVertex3f(-1.0f,-1.0f, 1.0f);					// Bottom Left Of The Quad (Front)
+		    GL11.glVertex3f( 1.0f,-1.0f, 1.0f);					// Bottom Right Of The Quad (Front)
+		    
+		    GL11.glColor3f(1.0f,1.0f,0.0f);						// Set The Color To Yellow
+		    GL11.glVertex3f( 1.0f,-1.0f,-1.0f);					// Top Right Of The Quad (Back)
+		    GL11.glVertex3f(-1.0f,-1.0f,-1.0f);					// Top Left Of The Quad (Back)
+		    GL11.glVertex3f(-1.0f, 1.0f,-1.0f);					// Bottom Left Of The Quad (Back)
+		    GL11.glVertex3f( 1.0f, 1.0f,-1.0f);					// Bottom Right Of The Quad (Back)
+		    
+		    GL11.glColor3f(0.0f,0.0f,1.0f);						// Set The Color To Blue
+		    GL11.glVertex3f(-1.0f, 1.0f, 1.0f);					// Top Right Of The Quad (Left)
+		    GL11.glVertex3f(-1.0f, 1.0f,-1.0f);					// Top Left Of The Quad (Left)
+		    GL11.glVertex3f(-1.0f,-1.0f,-1.0f);					// Bottom Left Of The Quad (Left)
+		    GL11.glVertex3f(-1.0f,-1.0f, 1.0f);					// Bottom Right Of The Quad (Left)
+		    
+		    GL11.glColor3f(1.0f,0.0f,1.0f);						// Set The Color To Violet
+		    GL11.glVertex3f( 1.0f, 1.0f,-1.0f);					// Top Right Of The Quad (Right)
+		    GL11.glVertex3f( 1.0f, 1.0f, 1.0f);					// Top Left Of The Quad (Right)
+		    GL11.glVertex3f( 1.0f,-1.0f, 1.0f);					// Bottom Left Of The Quad (Right)
+		    GL11.glVertex3f( 1.0f,-1.0f,-1.0f);					// Bottom Right Of The Quad (Right)
+        GL11.glEnd;
+        GL11.glPopMatrix;
     }
     
     def processInput {
         if(Display.isCloseRequested || Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)) isRunning = false;
         
-        if(Keyboard.isKeyDown(Keyboard.KEY_Q)) rot.x+=0.1f;
-        if(Keyboard.isKeyDown(Keyboard.KEY_E)) rot.x-=0.1f;
-        if(Keyboard.isKeyDown(Keyboard.KEY_A)) rot.y+=0.1f;
-        if(Keyboard.isKeyDown(Keyboard.KEY_D)) rot.y-=0.1f;
-        if(Keyboard.isKeyDown(Keyboard.KEY_Y)) rot.z+=0.1f;
-        if(Keyboard.isKeyDown(Keyboard.KEY_C)) rot.z-=0.1f;
+        if(Keyboard.isKeyDown(Keyboard.KEY_Q)) cam.rot.x+=0.7f;
+        if(Keyboard.isKeyDown(Keyboard.KEY_E)) cam.rot.x-=0.7f;
+        if(Keyboard.isKeyDown(Keyboard.KEY_A)) cam.rot.y+=0.7f;
+        if(Keyboard.isKeyDown(Keyboard.KEY_D)) cam.rot.y-=0.7f;
+        if(Keyboard.isKeyDown(Keyboard.KEY_Y)) cam.rot.z+=0.7f;
+        if(Keyboard.isKeyDown(Keyboard.KEY_C)) cam.rot.z-=0.7f;
 
-        if(Keyboard.isKeyDown(Keyboard.KEY_W)) tran.x+=0.1f;
-        if(Keyboard.isKeyDown(Keyboard.KEY_R)) tran.x-=0.1f;
-        if(Keyboard.isKeyDown(Keyboard.KEY_S)) tran.y+=0.1f;
-        if(Keyboard.isKeyDown(Keyboard.KEY_F)) tran.y-=0.1f;
-        if(Keyboard.isKeyDown(Keyboard.KEY_X)) tran.z+=0.1f;
-        if(Keyboard.isKeyDown(Keyboard.KEY_V)) tran.z-=0.1f;
+        if(Keyboard.isKeyDown(Keyboard.KEY_W)) cam.pos.x+=0.7f;
+        if(Keyboard.isKeyDown(Keyboard.KEY_R)) cam.pos.x-=0.7f;
+        if(Keyboard.isKeyDown(Keyboard.KEY_S)) cam.pos.y+=0.7f;
+        if(Keyboard.isKeyDown(Keyboard.KEY_F)) cam.pos.y-=0.7f;
+        if(Keyboard.isKeyDown(Keyboard.KEY_X)) cam.pos.z+=0.7f;
+        if(Keyboard.isKeyDown(Keyboard.KEY_V)) cam.pos.z-=0.7f;
         
+        if(Keyboard.isKeyDown(Keyboard.KEY_LEFT))  terrain.pos.x+=0.7f;
+        if(Keyboard.isKeyDown(Keyboard.KEY_RIGHT)) terrain.pos.x-=0.7f;
+        if(Keyboard.isKeyDown(Keyboard.KEY_UP))    terrain.pos.z+=0.7f;
+        if(Keyboard.isKeyDown(Keyboard.KEY_DOWN))  terrain.pos.z-=0.7f;
+
         if(Keyboard.isKeyDown(Keyboard.KEY_P)) {
-            println(tran.x+" "+tran.y+" "+tran.z);
-            println(rot.x+" "+rot.y+" "+rot.z);
+            println(cam.toString);
+            println(terrain.toString);
         }
     }    
 }
--- src/Geometry.scala
+++ src/Geometry.scala
@@ -1,35 +1,97 @@
 import org.lwjgl._
 import org.lwjgl.opengl._
+import org.lwjgl.util.glu._
 
 class Vec3(var x:Float, var y:Float, var z:Float) {
     def this() = this(0f,0f,0f);
-};
+    
+    override def toString = "%.2f, %.2f, %.2f".format(x,y,z);
+}
 class Quad(var p1:Vec3, var p2:Vec3, var p3:Vec3, var p4:Vec3) {
     def getPoints = List(p1,p2,p3,p4);
 }
 
-// @most models will be static - compile them into displaylists
+// @most models will be static - compile them into displaylists / VBOs
 // lazily on first render vs loading time?
 // catapult as model-chain - has static parts which move
 
 abstract class Model {
     var points:Array[Vec3];
-    var rot = new Vec3(0f,0f,0f);
-    var pos = new Vec3(0f,0f,0f);
-    var scale = new Vec3(0f,0f,0f);
+    var (pos,rot,scale) = (new Vec3(0f,0f,0f), new Vec3(0f,0f,0f), new Vec3(0f,0f,0f));
 
-    def setRotation(x:Float,y:Float,z:Float) = { rot = new Vec3(x,y,z); }
     def setPosition(x:Float,y:Float,z:Float) = { pos = new Vec3(x,y,z); }
+    def setRotation(x:Float,y:Float,z:Float) = { rot = new Vec3(x,y,z); }
     def setScale(x:Float,y:Float,z:Float) = { scale = new Vec3(x,y,z); }
     
     // How do I render this model?
     def render;
+    
+    override def toString:String = "p:("+pos.toString+"), " + "r:("+rot.toString+"), " + "s:("+scale.toString+")";
 }
 
+class Camera extends Model {
+    var points:Array[Vec3] = null;
+    
+    //@ school template.. make nice :P
+    
+    // default projection 
+    var perspective = false
+    var (near,far) = (1f,30f) // near, far clipping plane
+    var (fov,aspectRatio) = (45f,4/3f) // perspective stuff
+    var (minX,minY,maxX,maxY) = (-1f,-1f, 1f, 1f) // ortho stuff
+
+    // set a perspective projection
+    def setPerspective(fv:Float, ar:Float, n:Float, f:Float) {
+        perspective=true
+        fov=fv
+        aspectRatio=ar
+        near=n
+        far=f
+    }
+  
+    // set an ortographic projection
+    def setOrtho(mx:Float, my:Float, Mx:Float, My:Float, n:Float, f:Float) {
+        perspective=false
+        minX=mx
+        minY=my
+        maxX=Mx
+        maxY=My
+        near=n
+        far=f
+    }
+  
+    override def render() {
+        // setup projection matrix stack
+        GL11.glMatrixMode(GL11.GL_PROJECTION);
+        GL11.glLoadIdentity();
+        if(perspective) {
+            // perspective projection
+            GLU.gluPerspective(fov,aspectRatio, near,far);
+        } else {
+            // orthographic projection 
+            GL11.glOrtho(minX,maxX, minY,maxY, near,far);
+        }
+
+        // model view stack 
+        GL11.glMatrixMode(GL11.GL_MODELVIEW);
+        GL11.glLoadIdentity();
+        
+        // setup view space; 
+        GL11.glTranslatef(pos.x, pos.y, pos.z);
+
+        if(rot.z!=0) GL11.glRotatef(rot.z, 0, 0, 1);
+        if(rot.y!=0) GL11.glRotatef(rot.y, 0, 1, 0);
+        if(rot.x!=0) GL11.glRotatef(rot.x, 1, 0, 0);
+    }
+}
+
+
+// @maybe normalize width&height to 1 and then scale?
 class QuadPatch extends Model {
-    var width = 1;
-    var points:Array[Vec3]=null;
-    var texPoints:Array[Vec3]=null;
+    var width = 1
+    var points:Array[Vec3]=null
+    var texPoints:Array[Vec3]=null
+    var displayList = -1
     
     // constructors
     // @take in random points and make it into a valid quadpatch
@@ -44,18 +106,34 @@ class QuadPatch extends Model {
         texPoints = tex;
     }
 
+    // @make a method to merge quadpatches (for infinite terrain)
+ 
     def render {
-        GL11.glBegin(GL11.GL_QUADS);
-        // Draw in clockwise - (00,01,11,10); must skip last point of line.
-        for(i <- 0 until points.length-width-1; if((i+1)%width > 0)) {
-            List(points(i), points(i+width), points(i+width+1), points(i+1)).foreach {
-                (p:Vec3) => {
-                    GL11.glColor3f(p.y, p.y, p.y);
-                    //GL11.glTexCoord2f(clockwise(j)._1,clockwise(j)._2);
-                    GL11.glVertex3f(p.x, p.y, p.z-3);
+        GL11.glPushMatrix;
+        
+        GL11.glTranslatef(pos.x, pos.y, pos.z);
+        GL11.glRotatef(1f, rot.x, rot.y, rot.z);
+
+        if (displayList == -1) {
+            displayList = GL11.glGenLists(1);
+            GL11.glNewList(displayList,GL11.GL_COMPILE_AND_EXECUTE);
+            GL11.glBegin(GL11.GL_QUADS);
+            // Draw in clockwise - (00,01,11,10); must skip last point of line.
+            for(i <- 0 until points.length-width-1; if((i+1)%width > 0)) {
+                List(points(i), points(i+width), points(i+width+1), points(i+1)).foreach {
+                    (p:Vec3) => {
+                        GL11.glColor3f(p.y/5, p.y, p.y/5);
+                        //GL11.glTexCoord2f(clockwise(j)._1,clockwise(j)._2);
+                        GL11.glVertex3f(p.x, p.y, p.z);
+                    }
                 }
             }
+            GL11.glEnd;
+            GL11.glEndList;
+        } else {
+            GL11.glCallList(displayList);
         }
-        GL11.glEnd;
-    }
+        
+        GL11.glPopMatrix;
+   }
 }
