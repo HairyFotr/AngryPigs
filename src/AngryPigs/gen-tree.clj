f7cf6ac1e544d153e05c6827fa5ebc8be4eeb5a8
Swizec Teller <swizec@swizec.com>
2011-04-16 18:42:34 +0200

    figured out data structure

--- src/AngryPigs/gen-tree.clj
+++ src/AngryPigs/gen-tree.clj
@@ -3,3 +3,28 @@
 ; main program expects a list of point,vector tuples that represent cyllindres making up the tree
 
 ; operative keyword: fractals
+
+; data structure:
+; node: (x y z d)  (vector with distance)
+; tree: (node
+;        (node
+; 	(node
+; 	 (node) (node) (node) (node) (node))
+; 	(node
+; 	 (node) (node) (node) (node) (node))
+; 	(node
+; 	 (node) (node) (node) (node) (node)))
+;        (node
+; 	(node
+; 	 (node) (node) (node) (node) (node))
+; 	(node
+; 	 (node) (node) (node) (node) (node))
+; 	(node
+; 	 (node) (node) (node) (node) (node))))
+
+
+; generate a tree from starting position v
+(defn give-me-tree [point l]
+  (map #(+ %1 l) point))
+
+(println (give-me-tree [1 1 1] 5))
