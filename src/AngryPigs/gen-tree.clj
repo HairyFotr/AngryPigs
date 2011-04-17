172422463679a653ad4eccd67dc95c9735edf9c8
Swizec Teller <swizec@swizec.com>
2011-04-17 16:34:37 +0200

    creating a nice plane from a vector

--- src/AngryPigs/gen-tree.clj
+++ src/AngryPigs/gen-tree.clj
@@ -1,4 +1,6 @@
 
+(ns AngryPig.gen-tree)
+
 ; the aim of this thing is to generate a tree
 ; main program expects a list of point,vector tuples that represent cyllindres making up the tree
 
@@ -23,8 +25,20 @@
 ; 	 (node) (node) (node) (node) (node))))
 
 
-; generate a tree from starting position v
-(defn give-me-tree [point l]
-  (map #(+ %1 l) point))
+(def primes [2 3 5 7 11 13 17 23 29])
+(def max-depth 9)
 
-(println (give-me-tree [1 1 1] 5))
+; generate a tree from starting position v
+(defn travel [node depth]
+  (let [d (last node)]
+    (concat (map #(+ %1 d) (butlast node)) [(/ d (nth primes depth))])))
+
+(defn plane [N]
+  (let [point (butlast (travel N 0))]
+    (concat (butlast N)
+	    [(apply + (map #(* (nth N %1) (nth point %1)) [0 1 2]))])))
+
+(defn give-me-tree [node depth]
+  (plane node))
+
+(println (give-me-tree [1 2 3 5] 0))
